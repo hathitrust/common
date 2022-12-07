6f25846368d712ea70fec3d7e3500f6244fe4eb2
Roger Espinosa <roger@umich.edu>
2022-12-07 09:59:40 -0500

    add required label; mark fields explicitly required

--- web/alicorn/js/utils.bundle.js
+++ web/alicorn/js/utils.bundle.js
@@ -20661,7 +20661,7 @@ head.ready(function () {
   }
 
   function default_dialog() {
-    var html = '<form class="form-horizontal">' + '<div class="control-group">' + '<label class="control-label" for="email">Email Address<br /><span class="label">Optional</span></label>' + '<div class="controls">' + '<input id="email" name="email" type="text" class="input-xlarge" placeholder="[Your email address]" />' + '</div>' + '</div>' + '<div class="control-group">' + '<label class="control-label" for="comments">Comments</label>' + '<div class="controls">' + '<textarea rows="5" id="comments" name="comments" required="required" class="input-xlarge" placeholder="Add your feedback here"></textarea>' + '</div>' + '</div>' + '</form>';
+    var html = '<form class="form-horizontal">' + '<div class="control-group">' + '<label class="control-label" for="email">Email Address</label>' + '<div class="controls">' + '<input id="email" name="email" type="text" class="input-xlarge" placeholder="[Your email address]" required="required" />' + '<span class="required"><i class="icomoon icomoon-info-circle" aria-hidden="true"></i> This field is required.</span>' + '</div>' + '</div>' + '<div class="control-group">' + '<label class="control-label" for="comments">Comments</label>' + '<div class="controls">' + '<textarea rows="5" id="comments" name="comments" required="required" class="input-xlarge" placeholder="Add your feedback here"></textarea>' + '<span class="required"><i class="icomoon icomoon-info-circle" aria-hidden="true"></i> This field is required.</span>' + '</div>' + '</div>' + '</form>';
     return html;
   }
 
--- web/alicorn/src/js/components/feedback.js
+++ web/alicorn/src/js/components/feedback.js
@@ -29,15 +29,17 @@ head.ready(function() {
     var html =
     '<form class="form-horizontal">' +
       '<div class="control-group">' +
-        '<label class="control-label" for="email">Email Address<br /><span class="label">Optional</span></label>' +
+        '<label class="control-label" for="email">Email Address</label>' +
         '<div class="controls">' +
-          '<input id="email" name="email" type="text" class="input-xlarge" placeholder="[Your email address]" />' +
+          '<input id="email" name="email" type="text" class="input-xlarge" placeholder="[Your email address]" required="required" />' +
+          '<span class="required"><i class="icomoon icomoon-info-circle" aria-hidden="true"></i> This field is required.</span>' + 
         '</div>' +
       '</div>' +
       '<div class="control-group">' +
         '<label class="control-label" for="comments">Comments</label>' +
         '<div class="controls">' +
           '<textarea rows="5" id="comments" name="comments" required="required" class="input-xlarge" placeholder="Add your feedback here"></textarea>' +
+          '<span class="required"><i class="icomoon icomoon-info-circle" aria-hidden="true"></i> This field is required.</span>' + 
         '</div>' +
       '</div>' +
     '</form>';
